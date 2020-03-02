73bf028668c30aab6b57bcd7c1a28506f120bdc8
Aaron Exley <aaronde314@gmail.com>
2020-03-02 17:19:20 -0500

    changed input styles to be a global style

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Roboto:700&display=swap"
             rel="stylesheet"
           />
-          <link rel="stylesheet" type="text/css" href="css/styles.css"/>
         </Head>
         <body>
           <Main />
