850267c9f4cd49319cf6969f14cc93ee0b017abb
jclabatt <jclabatt@gmail.com>
2020-03-16 18:41:28 -0400

    Created the Checkout Product Card Component

---
+++ components/CheckoutProduct.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from "../css/CheckoutProduct.module.css";
+
+export class CheckoutProduct extends React.Component{
+    render(){
+        return <div className={styles.CheckoutProduct}>
+            <div className={styles.CheckoutImage}>
+                <img src = ""></img>
+            </div>
+            <div className={styles.CheckoutInfo}>
+                <p>Product Name</p>
+                <p>$3.99</p>
+                <table className={styles.CheckoutSpecs}>
+                    <tr>
+                        <td>Colour:</td>
+                        <td>Black</td>
+                    </tr>
+                    <tr>
+                        <td>Size</td>
+                        <td>S</td>
+                    </tr>
+                    <tr>
+                        <td>Quantity:</td>
+                        <tb>1</tb>
+                    </tr>
+                </table>
+            </div>
+            <div className={styles.CheckoutPrice}>
+                $3.99
+            </div>
+        </div>
+    }
+}
+
+export default CheckoutProduct;
