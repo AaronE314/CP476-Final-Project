38466f5128241d19ea4ee34dd439d032ddc28924
Austin Bursey <burs5200@mylaurier.ca>
2020-03-17 14:59:50 -0400

    Added Examples of Get and Post requests

---
+++ pages/api/getProducts.js
@@ -0,0 +1,39 @@
+import nextConnect from 'next-connect';
+import middleware from '../../middleware/database';
+import {ObjectID} from 'mongodb';
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.get(async (req, res) => {
+    try{
+        const { productType } = req.query;
+        let doc = {}
+        doc = await req.db.collection('StoreProducts').find({"name":req.query.productType}).toArray();
+        
+        
+        //FOR DEBUGGING
+        // console.log("14");
+        // if (doc.length > 0) { 
+            
+        //     doc.forEach(element => {
+        //         console.log(element)
+        //     });
+        //     console.log ("printing index 0 : ");
+        //      console.log(doc[0]); 
+        // }else {
+        //     console.log("doc.length = " + doc.length); 
+        // }
+        // console.log("15");
+        
+        
+        res.json(doc)
+    }catch(err){
+        throw err; 
+    }
+
+})
+
+
+export default (req, res) => handler.apply(req, res) 
