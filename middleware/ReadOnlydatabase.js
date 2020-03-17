d30e557662a9fb01cb355a7d237f3797d6ae82bf
Austin Bursey <burs5200@mylaurier.ca>
2020-03-17 15:16:11 -0400

    getMethods now use a read only user

---
+++ middleware/ReadOnlydatabase.js
@@ -0,0 +1,24 @@
+import { MongoClient } from 'mongodb';
+import nextConnect from 'next-connect';
+
+const client = new MongoClient("mongodb+srv://DBConnectUser:H39SSlEGZhTFE0kq@cluster0-tvjb2.mongodb.net/test?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function database(req, res, next) {
+  try {
+    if (!client.isConnected()) await client.connect().catch(function(err){throw err; });
+    req.dbClient = client;
+    req.db = client.db('CP476Main');
+    return next();
+  }catch(err){
+    throw err; 
+  }
+}
+
+const middleware = nextConnect();
+
+middleware.use(database);
+
+export default middleware;
