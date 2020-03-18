78111013a5452a77c89afdb9f034d86d7be6cae1
Aaron <aaronde314@gmail.com>
2020-03-17 20:27:31 -0400

    home page done

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import Head from 'next/head'
-import { Dialog } from '../components/Dialog'
 import { Footer } from '../components/Footer'
 import TopNav from '../components/TopNav'
+import React from 'react'
 
-import {Product} from "../components/Product"
+//const Home = () => (
+export class Home extends React.Component {
 
-const Home = () => (
-  <div className="container">
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      slogan: "A GOOD PLACE FOR A COMPANY SLOGAN"
+    }
+  }
+
+  render() {
+  return <div className="container">
     <Head>
       <title>Web Store</title>
       <link rel="icon" href="/favicon.ico" />
@@ -18,187 +27,123 @@ const Home = () => (
     </header>
 
     <main>
-  
-      {/* <Product></Product> */}
-      {/* <Dialog></Dialog> */}
-      
-      {/* <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
+      <div className="background">
+        
+        <img src="/images/img1.jpg" className="backgroundImage img1 noselect"/>
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <img src="/images/img2.jpg" className="backgroundImage img2 noselect"/>
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+      </div>
 
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
+      <div className="centerText">
+        <h1 className ="slogan">{this.state.slogan}</h1>
+        <div className="buttons">
+          <button className="shopNew">
+            SHOP NEW
+          </button>
+          <button className="shopNew">
+            FEATURED
+          </button>
+          <button className="shopNew">
+            SALE
+          </button>
+        </div>
+      </div>
 
-        <a
-          href="https://zeit.co/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div> */}
     </main>
 
     <footer>
-      {/* <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a> */}
       <Footer></Footer>
     </footer>
 
-    {/* <style jsx>{`
+    <style jsx>{`
+
       .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
+        position: relative;
+        height: calc(100vh + 310px) !important;
       }
 
       main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
+        position: relative;
+        padding-top: 80px;
+        height: calc(100vh - 80px);
       }
 
-      footer {
+      .centerText {
+        width: 728px;
+        position: absolute;
+        top: calc(50% - 94px/2);
+        left: calc(50% - 728px/2);
+      }
+
+      .background {
+        height: 100%;
         width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
+      }
+
+      .buttons {
         display: flex;
-        justify-content: center;
-        align-items: center;
+        justify-content: space-between;
+        width: 100%;
       }
 
-      footer img {
-        margin-left: 0.5rem;
+      .buttons button:hover {
+          filter: brightness(95%);
+          cursor: pointer;
       }
 
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+      .noselect { 
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        user-drag: none;
+      } 
+
+      .buttons button {
+
+          border: none;
+
+          width: 225px;
+          height: 32px;
+
+          margin-top: 32px;
+
+          left: calc(50% - 264px/2);
+
+          background: var(--highlightColor);
+
       }
 
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
+      .slogan {
+        width: 100%;
+        font-size: 32px;
         text-align: center;
+        text-transform: uppercase;
+        background: var(--highlightColor);
+        line-height: 37px;
       }
 
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
+      .backgroundImage {
+        --margin: 32px;
+        margin: var(--margin);
+        height: calc(100% - (2 * var(--margin)));
+        width: calc(50% - (var(--margin) + var(--margin)/2));
       }
 
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+      .img1 {
+        background: #CDC8E0;
+        margin-right: 0;
       }
 
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
+      .img2 {
+        background: #F5E1FF;
       }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style> */}
-
+    
+    `}</style>
     <style jsx global>{`
 
       * {
@@ -232,6 +177,7 @@ const Home = () => (
 
     `}</style>
   </div>
-)
+  }
+}
 
 export default Home
