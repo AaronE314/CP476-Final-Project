8d01eaad4315c27cdbd995ef88b75f92c59d0a21
Aaron <aaronde314@gmail.com>
2020-04-17 22:22:40 -0400

    orders page works, with authentication

---
+++ pages/orders.js
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from 'react';
+import Layout from '../components/layout';
+import Order from '../components/order';
+import { isSignedIn } from '../lib/userAuth';
+import Router from 'next/router';
+
+const Orders = ({orders}) => {
+
+    // const [orders, setOrders] = useState();
+
+
+
+    return <Layout fullPage={false}>
+        
+        <div className="orders">
+            {orders.map((order, i) => {
+                return <Order order={order} key={i}/>
+            })}
+        </div>
+        
+
+        <style jsx>{`
+
+            .orders {
+                margin: 0 192px;
+            }
+
+            @media only screen and (max-width: 1000px) {
+                
+                .orders {
+                    margin: 0 120px;
+                }
+
+            }
+
+            @media only screen and (max-width: 610px) {
+
+                .orders {
+                    margin: 45px 25px 0 25px;
+                }
+
+            }
+
+        `}</style>
+
+    </Layout>
+
+};
+
+Orders.getInitialProps = async (ctx) => {
+
+    console.log("init orders");
+
+    if (await isSignedIn()) {
+
+        // TODO: get orders for user
+        return {
+            orders: [
+            {
+                orderId: "1234567890",
+                date: "27/03/2020",
+                cost: "229.60",
+                Quantity: 17,
+                Address: "123 Street Ave. Toronto, ON",
+                productImages: [
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    
+                ]
+            },
+            {
+                orderId: "0987654321",
+                date: "20/02/2020",
+                cost: "70.49",
+                Quantity: 5,
+                Address: "123 Street Ave. Toronto, ON",
+                productImages: [
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    '/images/tempImages/tempImg1_1.jpg',
+                    
+                ]
+            }
+        ]};
+    } else {
+        console.log("oops");
+        if (ctx.res) {
+            console.log("server");
+            ctx.res.writeHead(302, {
+                Location: "/",
+                'Content-Type': 'text/html; charset=utf-8',
+            });
+            ctx.res.end();
+        } else {
+            Router.push("/");
+        }
+    }
+
+    return {};
+
+}
+
+
+
+
+export default Orders;
