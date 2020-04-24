dad6b227eea0dc1d4c10738065092dded87d5b7f
Aaron <aaronde314@gmail.com>
2020-04-24 15:07:37 -0400

    hopefully gh-pages works

--- constants.js
+++ constants.js
@@ -1,1 +1,4 @@
-export const URLString = "http://localhost:3000"; 
+const isProd = (process.env.NODE_ENV || 'production') === 'production'
+
+export const URLString = (process.env.NODE_ENV || 'production') === 'production'
+? "https://aarone314.github.io/CP476-Final-Project/" : "http://localhost:3000"; 
