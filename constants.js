2163b8e6a813901e13277c929db0fb5f82741ddc
Aaron <aaronde314@gmail.com>
2020-04-24 20:09:48 -0400

    small tweak for deploy

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 const isProd = (process.env.NODE_ENV || 'production') === 'production'
 
 export const URLString = (process.env.NODE_ENV || 'production') === 'production'
-? "https://aarone314.github.io/CP476-Final-Project/" : "http://localhost:3000"; 
+? "https://cp476groupproject.herokuapp.com" : "http://localhost:3000"; 
